4dea07a52b51a8a596ca8281854b23912beab635
Trystan Eveno <trystan.eveno@lephare.com>
2017-09-28 12:09:45 +0200

    orbitscontrol

--- src/application.js
+++ src/application.js
@@ -1,102 +1,37 @@
+import * as THREE from 'three';
+import threeOrbitControls from './utils/OrbitControls';
 import Stats from 'stats.js';
-import Particle from './Particle';
 
 import './style.css';
 
-const canvas = document.querySelector('#canvas');
-const count = document.querySelector('#count');
-const label = document.querySelector('#label');
-const resetPart = document.querySelector('.reset');
-const ctx = canvas.getContext('2d');
-const WIRE_DIST = 200;
-const MAX_PARTICLES = 2000;
-const NB_PARTICLES = 150;
-const NB_ADDED = 50;
-const particles = [];
-
-var mouseX = 9999;
-var mouseY = 9999;
+const OrbitControls = threeOrbitControls(THREE);
 
 const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+// scene, renderer, camera, mesh(geometry + material)
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domEvent);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    'color': 'red',
+});
+const mesh = new THREE.Mesh(geometry, material);
+
+camera.position.z = 10;
+scene.add(mesh);
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
 const animate = () => {
     stats.begin();
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    particles.forEach(p => {
-        p.draw(ctx);
-        p.drawLine(ctx, mouseX, mouseY, WIRE_DIST);
-        p.update(canvas);
-    });
-
+    renderer.render(scene, camera);
     stats.end();
     requestAnimationFrame(animate);
 };
 
-const addParticles = (e) => {
-    if(particles.length + NB_ADDED <= MAX_PARTICLES) {
-        for (var i = 0; i < NB_ADDED ; i++) {
-            particles.push(new Particle(canvas, e.x, e.y, randomColor()));
-        }
-
-        setCounter();
-    } else {
-        playDenisBrogniart();
-        document.querySelector("canvas").style.backgroundImage = "url('./public/ah.gif')";
-        setTimeout(() => {
-            document.querySelector("canvas").removeAttribute("style");
-        }, 900);
-    }
-};
-
-const create = () => {
-    for (var i = 0; i < NB_PARTICLES; i++) {
-        particles.push(new Particle(canvas, null, null, randomColor()));
-    }
-
-    setCounter();
-};
-
-const mousePosition = (e) => {
-    mouseX = e.x;
-    mouseY = e.y;
-};
-
-const playDenisBrogniart = () => {
-    var audio = document.getElementById("ah");
-
-    audio.volume = 1;
-    audio.play();
-}
-
-const randomColor = () => {
-    return '#'+Math.floor(Math.random() * 15456415 - 1000).toString(16);
-};
-
-const reset = () => {
-    particles.splice(NB_PARTICLES);
-
-    setCounter();
-};
-
-const resize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-};
-
-const setCounter = () => {
-    count.innerText = particles.length;
-    label.innerText = particles.length > 1 ? 'particles' : 'particle';
-};
-
-create();
-resize();
-requestAnimationFrame(animate);
-
-document.body.appendChild( stats.dom );
-document.querySelector("body").style.backgroundImage = "url('./public/ah.gif')"; // need to put gif in cache
-window.addEventListener('resize', resize);
-canvas.addEventListener('click', addParticles);
-canvas.addEventListener('mousemove', mousePosition);
-resetPart.addEventListener('click', reset);
+animate();
